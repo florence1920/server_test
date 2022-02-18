d68e2d9969596e6a8154e4494d053e5e32930e8e
lsh <jetaime12121@gmail.com>
2022-02-19 05:04:29 +0900

    re

---
+++ front/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <button @click="foo">submit</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  methods: {
+    foo() {
+      this.$http.get('/users')
+    }
+  },
+}
+</script>
